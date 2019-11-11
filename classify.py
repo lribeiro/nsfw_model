51b7698de0e19a04d8b84f75285d943c43fc4fe5
Luis Landeiro Ribeiro <ribeiro.luis@gmail.com>
2019-11-11 13:16:52 +0000

    test classify method

--- classify.py
+++ classify.py
@@ -1,7 +1,17 @@
 import sys
 from nsfw_detector import NSFWDetector
-detector = NSFWDetector('./nsfw.299x299.h5')
-detector_mobilenet = NSFWDetector('./nsfw_mobilenet2.224x224.h5')
+detector = NSFWDetector('../models/nsfw.299x299.h5')
+detector_mobilenet = NSFWDetector('../models/nsfw_mobilenet2.224x224.h5')
 
-print(detector.predict(sys.argv[0]))
+for file in sys.argv[1:]:
+    try:
+         classification = detector.predict(file,image_size=(299,299))
+         score = classification[file]
+         #if score['porn'] > 0.9:
+         print(file)
+         display(Image(filename = file, width=200, height=200))
+         print(score)
+    except:
+        pass
+            #traceback.print_exc(file=sys.stdout)
 #print(detector_mobilenet.predict(sys.argv))
